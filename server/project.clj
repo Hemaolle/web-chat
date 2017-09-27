05c554925258703e9ecfcec0ef9c5c09fadb0127
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-09-27 11:10:28 +0300

    Server with one placeholder route

--- server/project.clj
+++ server/project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.4.0"]
+                 [metosin/ring-http-response "0.6.5"]]
+  :main server.core)
--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,6 +1,19 @@
-(ns server.core)
+(ns server.core
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :as compojure]
+            [ring.util.http-response :as response]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn response-handler [request]
+  (response/ok
+    (str "<html><body> your IP is: "
+         (:remote-addr request)
+         "</body></html>")))
+
+(compojure/defroutes handler
+ (compojure/GET "/" request response-handler))
+
+(defn -main []
+  (jetty/run-jetty
+      (-> #'handler)
+      {:port 3000
+       :join? false}))
