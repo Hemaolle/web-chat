9e2cecb8cf0556f82c53651cfef2a7be2524d187
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-10-13 11:46:36 +0300

    Fix Propmt warning
    The Propmt was causing a warning for changing the input field from
    uncontrolled to controlled. This was because the default value was
    passed in the props with the key "value" instead of "defaultValue".

--- client/src/Prompt.jsx
+++ client/src/Prompt.jsx
@@ -8,7 +8,7 @@ class Prompt extends React.Component {
         super(props);
 
         this.state = {
-            value: this.props.defaultValue
+            value: this.props.value
         };
 
         this.onChange = (e) => this._onChange(e);
