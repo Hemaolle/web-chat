397b0f09815c3b66744ed655c81dc82960ae1cd0
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-10-17 17:24:20 +0300

    Set min lein version to "2.7.0"
    Required for Heroku to use a compatible Leiningen version.

--- server/project.clj
+++ server/project.clj
@@ -1,6 +1,7 @@
 (defproject server "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.7.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
