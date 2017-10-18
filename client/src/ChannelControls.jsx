aa39e0514235b0b360f03148ddade885b624114e
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-10-18 12:38:53 +0300

    Extract ChannelControls from Channels

---
+++ client/src/ChannelControls.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Popup from 'react-popup';
+
+class ChannelControls extends Component {
+  constructor(props) {
+    super(props);
+    this.promptChannelName = this.promptChannelName.bind(this);
+    this.promptChannelToJoin = this.promptChannelToJoin.bind(this);
+    this.memberIn = this.memberIn.bind(this);
+  }  
+
+  promptChannelName() {
+    Popup.plugins().prompt('Name the new channel', '', 'Type the channel name', function (value) {
+      this.props.onChannelAdd({name: value});
+    }.bind(this));
+  }
+
+  promptChannelToJoin() {
+    var availableChannels = this.props.allChannels.filter(
+      (channel) => !this.memberIn(channel));
+    var options = availableChannels.map((channel) =>
+      ({ value: channel.id, label: channel.name }));
+
+    if (availableChannels.length === 0) {
+      Popup.alert('You\'ve already joined all the channels.');
+      return;
+    }
+    
+    Popup.plugins().select('Select a channel to join', options,
+      (channelId) => this.props.onChannelJoin(channelId));
+  }
+
+  memberIn(channel) {
+    return this.props.myChannels.some(myChannel => myChannel.id === channel.id);
+  }
+
+  render() {
+    // Don't show the button unless we have the required list of channels.
+    var joinChannelButton = null;
+    if (this.props.allChannels)
+    { 
+      joinChannelButton = 
+        (<button onClick={this.promptChannelToJoin}>Join channel</button>);
+    }
+
+    return (
+      <div>
+        {joinChannelButton}
+         <button onClick={this.promptChannelName}>New channel</button>
+      </div>);
+  }
+}
+
+export default ChannelControls;
--- client/src/Channels.jsx
+++ client/src/Channels.jsx
@@ -1,42 +1,14 @@
 import React, { Component } from 'react';
-import Popup from 'react-popup';
+import ChannelControls from './ChannelControls.jsx'
 
 class Channels extends Component {
   constructor(props) {
     super(props);
-    this.promptChannelName = this.promptChannelName.bind(this);
     this.selectChannel = this.selectChannel.bind(this);
-    this.promptChannelToJoin = this.promptChannelToJoin.bind(this);
-    this.memberIn = this.memberIn.bind(this);
-  }
-
-  promptChannelName() {
-    Popup.plugins().prompt('Name the new channel', '', 'Type the channel name', function (value) {
-      this.props.onChannelAdd({name: value});
-    }.bind(this));
-  }
+  }  
 
   selectChannel(channel) {
     this.props.onChannelSelect(channel)
-  }
-
-  promptChannelToJoin() {
-    var availableChannels = this.props.allChannels.filter(
-      (channel) => !this.memberIn(channel));
-    var options = availableChannels.map((channel) =>
-      ({ value: channel.id, label: channel.name }));
-
-    if (availableChannels.length === 0) {
-      Popup.alert('You\'ve already joined all the channels.');
-      return;
-    }
-    
-    Popup.plugins().select('Select a channel to join', options,
-      (channelId) => this.props.onChannelJoin(channelId));
-  }
-
-  memberIn(channel) {
-    return this.props.myChannels.some(myChannel => myChannel.id === channel.id);
   }
 
   render() {
@@ -52,22 +24,18 @@ class Channels extends Component {
         </li>);
     }
 
-    // Don't show the button unless we have the required list of channels.
-    var joinChannelButton = null;
-    if (this.props.allChannels)
-    { 
-      joinChannelButton = 
-        (<button onClick={this.promptChannelToJoin}>Join channel</button>);
-    }
-
     return (
       <div className="Channels">
         <h3>Channels</h3>
         <ul>
           {channelItems}
         </ul>
-        {joinChannelButton}
-        <button onClick={this.promptChannelName}>New channel</button>
+        <ChannelControls
+          myChannels={this.props.myChannels}
+          allChannels={this.props.allChannels}
+          onChannelSelect={this.props.onChannelSelect}
+          onChannelAdd={this.props.onChannelAdd}
+          onChannelJoin={this.props.onChannelJoin}/>
       </div>
     );
   }
