8c36535260cd663e1a12d053a800a45f622daee6
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-10-18 11:56:00 +0300

    App.js -> App.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Popup from 'react-popup';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import './popup-styles.css'
 
